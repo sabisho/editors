46707e699d36b269a20e598b4664eb487b810e26
helixoid <ayushrajmth91@gmail.com>
2026-06-15 09:32:14 +0530

    Added mini.nvim config.

---
+++ nvim/lua/plugin-configs/mini.lua
@@ -0,0 +1,6 @@
+require('mini.ai').setup()
+require('mini.icons').setup()
+require('mini.notify').setup()
+require('mini.pairs').setup()
+require('mini.statusline').setup()
+require('mini.surround').setup()
