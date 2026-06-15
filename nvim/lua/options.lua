5fb6d3536a003508a5540628dd4888d196151075
helixoid <ayushrajmth91@gmail.com>
2026-06-15 09:11:39 +0530

    Added ui2 for neovim.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -8,7 +8,7 @@ local opt = vim.opt
 opt.number = true
 opt.relativenumber = true
 opt.cursorline = true
-opt.signcolumn = "yes"
+opt.signcolumn = 'yes'
 
 -- Tab Settings
 opt.tabstop = 2
@@ -24,9 +24,9 @@ opt.incsearch = true
 opt.hlsearch = true
 
 -- QOL Settings
-opt.winborder = "rounded"
+opt.winborder = 'rounded'
 opt.showmode = false
-opt.clipboard = "unnamedplus"
+opt.clipboard = 'unnamedplus'
 opt.timeoutlen = 300
 opt.splitbelow = true
 opt.splitright = true
@@ -36,28 +36,31 @@ opt.termguicolors = true
 opt.undofile = true
 opt.list = true
 opt.listchars = {
-  tab = "» ",
-  trail = "·",
-  nbsp = "␣",
+  tab = '» ',
+  trail = '·',
+  nbsp = '␣',
 }
-opt.inccommand = "split"
+opt.inccommand = 'split'
 opt.scrolloff = 10
-opt.foldmethod = "indent"
+opt.foldmethod = 'indent'
 opt.foldlevel = 99
 opt.foldenable = false
 
+-- Enable the experimental UI2 subsystem
+require('vim._core.ui2').enable {}
+
 -- Diagnostics Icons in the gutter
-vim.diagnostic.config({
+vim.diagnostic.config {
   signs = {
     text = {
-      [vim.diagnostic.severity.ERROR] = "",
-      [vim.diagnostic.severity.WARN] = "",
-      [vim.diagnostic.severity.HINT] = "󰌵",
-      [vim.diagnostic.severity.INFO] = "",
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.HINT] = '󰌵',
+      [vim.diagnostic.severity.INFO] = '',
     },
   },
   virtual_lines = {
     current_line = true,
   },
   virtual_text = false,
-})
+}
